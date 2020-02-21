fd61dcce7f46b8bbef0c46bd72b5b71b06e9a4b4
Patrick Grady <patickrgrady81@gmail.com>
2020-02-21 09:19:53 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
+  get '/newteam' do 
+    erb :newteam
+  end
+
+  post "/team" do 
+    @params = params
+    erb :team
+  end
 
 end
